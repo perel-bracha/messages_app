1d8882d488b5968489c7f84733037e206ce7813b
פרל ברכה נדל <pn0556781949@gmail.com>
2025-04-22 19:20:38 +0300

    home navigate 7

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ import { Home } from "./components/Home";
 import { LogIn } from "./components/LogIn";
 import { Navigate } from "react-router-dom";
 import { RotatingMessages } from "./components/ScreenClali";
-import { io } from "socket.io-client";
-const socket = io(`${process.env.REACT_APP_SERVER_URL}`);
+import {io} from 'socket.io-client'
+const socket=io(`${process.env.REACT_APP_SERVER_URL}`);
 function App() {
   const ProtectedRoute = ({ children }) => {
     const token = sessionStorage.getItem("token"); // Assuming token is stored in localStorage
@@ -38,58 +38,12 @@ function App() {
               </ProtectedRoute>
             }
           />
+          <Route path="*" element={<Navigate to="/home" />} />
         </Route>
-        <Route
-          path="/screen1"
-          element={<Screen screenNum={1} socket={socket} />}
-        />
-        <Route
-          path="/screen2"
-          element={<Screen screenNum={2} socket={socket} />}
-        />
-        <Route path="/screen3" element={<RotatingMessages socket={socket} />} />
-        <Route
-          path="*"
-          element={
-            <Routes>
-              <Route path="/" element={<Layout />}>
-                <Route index element={<Home />} />
-                <Route index path="/home" element={<Home />} />
-                <Route path="/login" element={<LogIn />} />
-                <Route
-                  path="/add_message"
-                  element={
-                    <ProtectedRoute>
-                      <AddMessage />
-                    </ProtectedRoute>
-                  }
-                />
-                <Route
-                  path="/messages_list"
-                  element={
-                    <ProtectedRoute>
-                      <MessagesList />
-                    </ProtectedRoute>
-                  }
-                />
-                <Route path="*" element={<Navigate to="/home" />} />
-              </Route>
-              <Route
-                path="/screen1"
-                element={<Screen screenNum={1} socket={socket} />}
-              />
-              <Route
-                path="/screen2"
-                element={<Screen screenNum={2} socket={socket} />}
-              />
-              <Route
-                path="/screen3"
-                element={<RotatingMessages socket={socket} />}
-              />
-              <Route path="*" element={<Navigate to="/home" />} />
-            </Routes>
-          }
-        />
+        <Route path="/screen1" element={<Screen screenNum={1} socket={socket}/>} />
+        <Route path="/screen2" element={<Screen screenNum={2} socket={socket}/>} />
+        <Route path="/screen3" element={<RotatingMessages socket={socket}/>} />
+        <Route path="*" element={<Navigate to="/home" />} />
       </Routes>
     </>
   );
