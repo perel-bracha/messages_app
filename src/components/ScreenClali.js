d3a91cff5826bcea11784c7dbc975a8cc9d54a30
perel-bracha <148152362+perel-bracha@users.noreply.github.com>
2025-04-23 15:15:15 +0300

    Update ScreenClali.js

--- src/components/ScreenClali.js
+++ src/components/ScreenClali.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-export function RotatingMessages({ interval = 5000, socket }) {
+export function RotatingMessages({ interval = 8000, socket }) {
   const [currentIndex, setCurrentIndex] = useState(0);
   const [pairs, setPairs] = useState([]);
   const [messages, setMessages] = useState([]);
